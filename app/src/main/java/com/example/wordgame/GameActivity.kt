3c36785c1344f04f584f5940ee2edc57c0866439
Eunseo Lee <el851@georgetown.edu>
2022-02-06 19:36:27 -0500

    Commit #2

--- app/src/main/java/com/example/wordgame/GameActivity.kt
+++ app/src/main/java/com/example/wordgame/GameActivity.kt
@@ -2,13 +2,68 @@ package com.example.wordgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.*
+import java.util.*
 
 class GameActivity : AppCompatActivity() {
-
-
+    var timer: TextView? = null
+    var guess: EditText? = null
+    var guessesLeft: TextView? = null
+    var score: TextView? = null
+    var checkButton: Button? = null
+    var answerButton: Button? = null
+    var nextButton: Button? = null
+    var linearLayout: LinearLayout? = null
+    var start = false
+    var howLong = 10000
+    var correct = 0
+    var totalProblems = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+        val bundle: Bundle? = intent.extras
+
     }//onCreate
+
+    fun updateTimer(secondsLeft : Int) {
+        val minutes = secondsLeft / 60
+        val seconds = secondsLeft - (minutes * 60)
+        var secondStr = seconds.toString()
+        if (seconds <= 9) {
+            secondStr = "0$secondStr"
+        }
+        timer!!.text =  minutes.toString() + ":" + secondStr
+    }//updateTimer
+
+    fun gameTimer() {
+        start = true
+        timer!!.text = "1:00"
+
+        object : CountDownTimer( howLong.toLong(), 1000) {
+            override fun onTick(p0: Long) {
+                updateTimer((p0 / 1000).toInt())
+                howLong -= 1000
+            }
+
+            override fun onFinish() {
+                timer!!.text = "0:00"
+                Toast.makeText(
+                    getApplicationContext(), "Your got " + correct + " out of " +
+                            totalProblems + " Correct", Toast.LENGTH_LONG
+                ).show()
+                start = false;
+            }
+
+        }
+
+
+
+
+
+    }//gameTimer
+
+
 }//GameActivity
